29be48de31381a835146501808ca84ef9b66f20c
Tomasz Goliński - laptop <tgolinski55@gmail.com>
2019-06-28 17:07:02 +0200

    Improve for chat

--- Biletomat/View/WyszukajKoncert.xaml.cs
+++ Biletomat/View/WyszukajKoncert.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -35,7 +36,7 @@ namespace Biletomat.View
         {
             var textBlock = new TextBlock();
             var border = new Border();
-            border.CornerRadius = new CornerRadius(10);
+            border.CornerRadius = new CornerRadius(10, 0, 10, 10);
             border.Child = textBlock;
             border.BorderBrush = Brushes.Black;
             border.BorderThickness = new Thickness(1);
@@ -60,7 +61,7 @@ namespace Biletomat.View
         {
             var textBlock = new TextBlock();
             var border = new Border();
-            border.CornerRadius = new CornerRadius(10);
+            border.CornerRadius = new CornerRadius(0, 10, 10, 10);
             border.Child = textBlock;
             border.BorderBrush = Brushes.Black;
             border.BorderThickness = new Thickness(1);
@@ -78,6 +79,63 @@ namespace Biletomat.View
 
         }
 
+        private void BotResultPreset(string[] result)
+        {
+            var nazwaSrch = "";
+            var dataSrch = "";
+            var miejsceSrch = "";
+
+            var dataGrid = new DataGrid();
+            var border = new Border();
+            var button = new Button();
+            var stackPanel = new StackPanel();
+            stackPanel.Children.Add(dataGrid);
+            stackPanel.Children.Add(button);
+
+            border.CornerRadius = new CornerRadius(0, 10, 10, 10);
+            border.Child = stackPanel;
+            border.BorderBrush = Brushes.Black;
+            border.BorderThickness = new Thickness(1);
+            border.Background = new SolidColorBrush(Color.FromRgb(36, 92, 127));
+            border.Margin = new Thickness(5);
+            border.MaxWidth = 300;
+            border.HorizontalAlignment = HorizontalAlignment.Left;
+
+            var col1 = new DataGridTextColumn();
+            var col2 = new DataGridTextColumn();
+            var col3 = new DataGridTextColumn();
+            col1.Header = "Nazwa";
+            col2.Header = "Data";
+            col3.Header = "Miejsce";
+            dataGrid.Columns.Add(col1);
+            dataGrid.Columns.Add(col2);
+            dataGrid.Columns.Add(col3);
+            foreach (var line in result)
+            {
+                nazwaSrch = line.Split('\t')[2];
+                dataSrch = line.Split('\t')[0];
+                miejsceSrch = line.Split('\t')[3];
+                dataGrid.ItemsSource = result;
+            }
+            col1.Binding = new Binding(nazwaSrch);
+            col2.Binding = new Binding(dataSrch);
+            col3.Binding = new Binding(miejsceSrch);
+            dataGrid.Margin = new Thickness(5);
+
+
+            button.Content = "Rezerwuj";
+            button.HorizontalAlignment = HorizontalAlignment.Right;
+            button.VerticalAlignment = VerticalAlignment.Bottom;
+
+            //textBlock.Text = str;
+            //textBlock.TextWrapping = TextWrapping.Wrap;
+            //textBlock.Foreground = Brushes.WhiteSmoke;
+            //textBlock.Padding = new Thickness(5);
+
+            chatField.Children.Add(border);
+
+        }
+
         private void GenerateResponse(string str)
         {
             string message = "";
@@ -86,11 +144,34 @@ namespace Biletomat.View
                 if (str.Contains("Lewicki"))
                     message = "Jak ja go kurwa nienawidze...";
                 else
-                    message = "Interesuje Cię zespół Karol Krawczyk? Oto lista koncertów tego zespołu";
+                    message = "Interesuje Cię zespół " + chatMessage.Text + "? Oto lista propozycji dla tego zapytania:";
 
                 BotMessagePreset(message);
+                GenerateResult();
             }
 
+        }
+
+        private void GenerateResult()
+        {
+            var search = chatMessage.Text;
+            List<string> result = new List<string>();
+            string path = @"ListaKoncertów.txt";
+            string[] lines = File.ReadAllLines(path);
+            int i = 0;
+            foreach (var line in lines)
+            {
+                if (line.ToLower().Contains(search.ToLower()))
+                    result.Add(line);
+
+
+                //var firstValue = line.Split('\t')[0];
+                //var secondValue = line.Split('\t')[1];
+                //var thirdValue = line.Split('\t')[2];
+                //var fourthValue = line.Split('\t')[3];
+                i++;
+            }
+            BotResultPreset(result.ToArray());
         }
     }
 }
